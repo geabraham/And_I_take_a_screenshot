20851474f6667c4a5cae7a0a15420814475d05cc
Kent Davy <kdavy@mdsol.com>
2014-10-27 12:34:58 -0700

    Deleted sandman stuff

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Minotaur::Application.routes.draw do
 
   root to: 'construction#index'
 
-  get '/sandman', to: 'sandman#index'
-  match '/sandman/:action', to: 'sandman#:action', via: 'get'
-
   get '/hollywood', to: 'hollywood#index'
   match '/hollywood/:action', to: 'hollywood#:action', via: 'get'
 
--- test/unit/helpers/sandman_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class SandmanHelperTest < ActionView::TestCase
-end
