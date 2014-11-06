5f80d6623208d639cddff4956d27bc849734d63a
Todd <tmartino@mdsol.com>
2014-11-06 12:51:33 -0500

    adding spec for template rendering

--- spec/controllers/patient_registrations_controller_spec.rb
+++ spec/controllers/patient_registrations_controller_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 describe PatientRegistrationsController do
   
   describe "GET 'index'" do
+    it 'uses the patient_registration template' do
+      get :index
+      expect(response).to render_template("patient_registration")
+    end
+    
     it 'returns http success' do
       expect(response).to be_success
     end
