779cde0a113cd741f43253cba91b2244e5b341ed
Todd <tmartino@mdsol.com>
2014-11-10 15:21:24 -0500

    post to /activate on entry of entire code

--- app/assets/javascripts/patient_flow/activation_code.js
+++ app/assets/javascripts/patient_flow/activation_code.js
@@ -4,8 +4,14 @@ $(".code").keyup(function(e) {
   if( str != "" ) {
     var regx = /^[A-Za-z0-9]+$/;
     $(this).val($(this).val().toUpperCase());
-    if (regx.test(str)) {
+    
+    if(regx.test(str)) {
       $(".validation_error").hide();
+      
+      // submit if the full code has been filled in
+      if(str.length == 6) {
+        $.post( "/activate", { activation_code: str });
+      }
     }
     else {
       $(".validation_error").show();
