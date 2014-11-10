ab740b85bf0004151bafb9e0b338be880cddb63c
Todd <tmartino@mdsol.com>
2014-11-10 11:10:41 -0500

    add IDs to each input so they can be manipulated separately; upcase alphabetical inputs

--- app/assets/javascripts/patient_flow/activation_code.js
+++ app/assets/javascripts/patient_flow/activation_code.js
@@ -1,14 +1,15 @@
 $(".code").keyup(function(e) {
   var str = $.trim( $(this).val() );
   if( str != "" ) {
-      var regx = /^[A-Za-z0-9]+$/;
-      if (!regx.test(str)) {
-        $(".validation_error").show();
-      }
-      else {
-        $(".validation_error").hide();
-      }
+    $(this).val(str.toUpperCase());
+    var regx = /^[A-Za-z0-9]+$/;
+    if (!regx.test(str)) {
+      $(".validation_error").show();
     }
+    else {
+      $(".validation_error").hide();
+    }
+  }
 });
 
 //TODO validate that ALL inputs are alphanumeric, not just one field
