24eb86069ce434b38b610d5cb13a6225bcc3bc19
Todd <tmartino@mdsol.com>
2014-11-10 11:25:17 -0500

    remove todo -- now validates all inputs

--- app/assets/javascripts/patient_flow/activation_code.js
+++ app/assets/javascripts/patient_flow/activation_code.js
@@ -1,15 +1,14 @@
 $(".code").keyup(function(e) {
-  var str = $.trim( $(this).val() );
+  var str = $.trim( $(".code#1").val() + $(".code#2").val() +$(".code#3").val() +
+                    $(".code#4").val() + $(".code#5").val() +$(".code#6").val());
   if( str != "" ) {
-    $(this).val(str.toUpperCase());
     var regx = /^[A-Za-z0-9]+$/;
-    if (!regx.test(str)) {
-      $(".validation_error").show();
+    $(this).val($(this).val().toUpperCase());
+    if (regx.test(str)) {
+      $(".validation_error").hide();
     }
     else {
-      $(".validation_error").hide();
+      $(".validation_error").show();
     }
   }
 });
-
-//TODO validate that ALL inputs are alphanumeric, not just one field
