a028e5a95a143a11cce1d93af9c95cb117321199
Todd <tmartino@mdsol.com>
2014-11-10 16:53:57 -0500

    getting input code values more concisely

--- app/assets/javascripts/patient_flow/activation_code.js
+++ app/assets/javascripts/patient_flow/activation_code.js
@@ -1,6 +1,9 @@
 $(".code").keyup(function(e) {
-  var str = $.trim( $("#code-1").val() + $("#code-2").val() +$("#code-3").val() +
-                    $("#code-4").val() + $("#code-5").val() +$("#code-6").val());
+  var str = "";
+  $(".code").each(function() {
+    str += $.trim($(this).val());
+  });
+  
   if( str !== "" ) {
     var regx = /^[A-Za-z0-9]+$/;
     $(this).val($(this).val().toUpperCase());
