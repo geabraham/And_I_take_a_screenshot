90ebd80d74bd4642304985dd0b4362cf97143b74
Todd <tmartino@mdsol.com>
2014-11-10 16:08:58 -0500

    better code input IDs

--- app/assets/javascripts/patient_flow/activation_code.js
+++ app/assets/javascripts/patient_flow/activation_code.js
@@ -1,6 +1,6 @@
 $(".code").keyup(function(e) {
-  var str = $.trim( $(".code#1").val() + $(".code#2").val() +$(".code#3").val() +
-                    $(".code#4").val() + $(".code#5").val() +$(".code#6").val());
+  var str = $.trim( $("#code-1").val() + $("#code-2").val() +$("#code-3").val() +
+                    $("#code-4").val() + $("#code-5").val() +$("#code-6").val());
   if( str !== "" ) {
     var regx = /^[A-Za-z0-9]+$/;
     $(this).val($(this).val().toUpperCase());
