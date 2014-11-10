c413a94707aa543b3d14c6965277832e71e67ff8
Todd <tmartino@mdsol.com>
2014-11-10 16:02:30 -0500

    strict comparison in activation_code.js

--- app/assets/javascripts/patient_flow/activation_code.js
+++ app/assets/javascripts/patient_flow/activation_code.js
@@ -1,7 +1,7 @@
 $(".code").keyup(function(e) {
   var str = $.trim( $(".code#1").val() + $(".code#2").val() +$(".code#3").val() +
                     $(".code#4").val() + $(".code#5").val() +$(".code#6").val());
-  if( str != "" ) {
+  if( str !== "" ) {
     var regx = /^[A-Za-z0-9]+$/;
     $(this).val($(this).val().toUpperCase());
     
@@ -9,7 +9,7 @@ $(".code").keyup(function(e) {
       $(".validation_error").hide();
       
       // submit if the full code has been filled in
-      if(str.length == 6) {
+      if(str.length === 6) {
         //TODO figure out what to stub out here for testing until the redirect
         //     to next screen is ready
         $.post( "/activate", { activation_code: str });
