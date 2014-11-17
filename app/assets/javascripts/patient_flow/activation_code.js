d7accbff2ed0280bcc580dd335f99dcd49ded63e
Todd <tmartino@mdsol.com>
2014-11-17 12:31:05 -0500

    took out last couple example specs, changes to get jQuery event triggers working, added test around keyup event

--- app/assets/javascripts/patient_flow/activation_code.js
+++ app/assets/javascripts/patient_flow/activation_code.js
@@ -1,25 +1,25 @@
-$(".code").keyup(function(e) {
-  $(this).val($(this).val().toUpperCase());
-  
-  str = getInputString();
-  
-  if( str !== "" ) {
-    var regx = /^[A-Za-z0-9]+$/;
+$(function() {
+  $(".code").live('keyup', function(e) {
+    $(this).val($(this).val().toUpperCase());
+    str = getInputString();
+    if( str !== "" ) {
+      var regx = /^[A-Za-z0-9]+$/;
     
-    if(regx.test(str)) {
-      $(".validation_error").hide();
+      if(regx.test(str)) {
+        $(".validation_error").hide();
       
-      // submit if the full code has been filled in
-      if(str.length === 6) {
-        //TODO figure out what to stub out here for testing until the redirect
-        //     to next screen is ready
-        $.post("/activation_codes/" + str + "/activate");
+        // submit if the full code has been filled in
+        if(str.length === 6) {
+          //TODO figure out what to stub out here for testing until the redirect
+          //     to next screen is ready
+          $.post("/activation_codes/" + str + "/activate");
+        }
+      }
+      else {
+        $(".validation_error").show();
       }
     }
-    else {
-      $(".validation_error").show();
-    }
-  }
+  });
 });
 
 function getInputString() {
--- spec/javascripts/activation_code_spec.js.coffee
+++ spec/javascripts/activation_code_spec.js.coffee
@@ -1,15 +1,18 @@
 describe 'activation code page', ->
-  it 'should pass', ->
-    expect(1).toEqual 1
-    return
-    
-  it "should have jasmine-jquery", ->
-    loadFixtures "activationCodeFixture.html"
-    expect($("#code_enter")).toHaveText "Please enter your activation code"
-    return
-    
   it 'determines the input string when one character is entered', ->
-    setFixtures '<input class="code" id="code-1" maxlength="1" value="f" />'
-    expect(getInputString()).toEqual 'f'
+    setFixtures '<input class="code" id="code-1" maxlength="1" value="7" />'
+    expect(getInputString()).toEqual '7'
+    return
+    
+  it 'determines the input string when multiple characters are entered', ->
+    loadFixtures 'activationCodeFixture.html'
+    expect(getInputString()).toEqual 'heyman'
+    return
+    
+  it 'calls getInputString() on keyup event', ->
+    loadFixtures 'activationCodeFixture.html'
+    window.getInputString = jasmine.createSpy('getInputString spy').and.returnValue('H')
+    $('.code').trigger 'keyup'
+    expect(window.getInputString).toHaveBeenCalled()
     return
   return
