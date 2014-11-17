137f2fe3f6330d271f5ce55c05873261150fa315
Todd <tmartino@mdsol.com>
2014-11-17 16:24:24 -0500

    remove comments

--- app/assets/javascripts/patient_flow/activation_code.js
+++ app/assets/javascripts/patient_flow/activation_code.js
@@ -8,10 +8,7 @@ $(function() {
       if(regx.test(str)) {
         $(".validation_error").hide();
       
-        // submit if the full code has been filled in
         if(str.length === 6) {
-          //TODO figure out what to stub out here for testing until the redirect
-          //     to next screen is ready
           $.post("/activation_codes/" + str + "/activate");
         }
       }
