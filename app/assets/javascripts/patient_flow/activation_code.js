ad8a9ddf8fe28440a1052c4c8abf4ba21b0094c6
Todd <tmartino@mdsol.com>
2014-11-18 12:37:48 -0500

    change to fixture/js so event testing works in jasmine

--- app/assets/javascripts/patient_flow/activation_code.js
+++ app/assets/javascripts/patient_flow/activation_code.js
@@ -1,23 +1,25 @@
 $(function() {
-  $(".code").live('keyup', function(e) {
-    $(this).val($(this).val().toUpperCase());
-    str = getInputString();
-    if( str !== "" ) {
-      var regx = /^[A-Za-z0-9]+$/;
+  $(".code").on('keyup', handleInput);
+});
+
+function handleInput() {
+  $(this).val($(this).val().toUpperCase());
+  str = getInputString();
+  if( str !== "" ) {
+    var regx = /^[A-Za-z0-9]+$/;
+  
+    if(regx.test(str)) {
+      $(".validation_error").hide();
     
-      if(regx.test(str)) {
-        $(".validation_error").hide();
-      
-        if(str.length === 6) {
-          $.post("/activation_codes/" + str + "/activate");
-        }
-      }
-      else {
-        $(".validation_error").show();
+      if(str.length === 6) {
+        $.post("/activation_codes/" + str + "/activate");
       }
     }
-  });
-});
+    else {
+      $(".validation_error").show();
+    }
+  }
+}
 
 function getInputString() {
   var inputString = "";
