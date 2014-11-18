bcc75597935413badec5d5c5a5966fb18ca59dc2
Todd <tmartino@mdsol.com>
2014-11-18 14:40:02 -0500

    make spec more spec-ific

--- spec/views/_activation_code.html.haml_spec.rb
+++ spec/views/_activation_code.html.haml_spec.rb
@@ -7,15 +7,20 @@ describe 'activation_codes/_activation_code.html.haml' do
     end
     
     it "displays the expected 'Please enter code' label" do
-      expect(rendered).to have_selector('.code_enter')
+      expect(rendered).to have_selector('h2.code_enter', text: 'Please enter your activation code')
     end
     
     it 'displays the expected instruction text' do
-      expect(rendered).to have_selector('.code_instruction')
+      expect(rendered).to have_selector('div.code_instruction', text: 'If you have already registered, please return to the Patient Cloud App. If you have not registered and do not have an activation code or email invite please contact your doctor.')
     end
     
     it 'displays the activation code input fields' do
-      expect(rendered).to have_selector('.code')
+      expect(rendered).to have_selector("input#code-1")
+      expect(rendered).to have_selector("input#code-2")
+      expect(rendered).to have_selector("input#code-3")
+      expect(rendered).to have_selector("input#code-4")
+      expect(rendered).to have_selector("input#code-5")
+      expect(rendered).to have_selector("input#code-6")
     end
   end
 end
