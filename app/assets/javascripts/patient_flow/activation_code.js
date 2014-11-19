239bfc18b72487fa18d419b95101ee2bd08afda0
Todd <tmartino@mdsol.com>
2014-11-19 12:51:09 -0500

    functions on doc ready

--- app/assets/javascripts/patient_flow/activation_code.js
+++ app/assets/javascripts/patient_flow/activation_code.js
@@ -1,30 +1,30 @@
 $(function() {
-  $(".code").on('keyup', handleInput);
-});
-
-var handleInput = function() {
-  $(this).val($(this).val().toUpperCase());
-  str = getInputString();
-  if( str !== "" ) {
-    var regx = /^[A-Za-z0-9]+$/;
+  var getInputString = function() {
+    var inputString = "";
+    $(".code").each(function() {
+      inputString += $.trim($(this).val());
+    });
+    return inputString;
+  }
   
-    if(regx.test(str)) {
-      $(".validation_error").hide();
+  var handleInput = function() {
+    $(this).val($(this).val().toUpperCase());
+    str = getInputString();
+    if( str !== "" ) {
+      var regx = /^[A-Za-z0-9]+$/;
+  
+      if(regx.test(str)) {
+        $(".validation_error").hide();
     
-      if(str.length === 6) {
-        $.post("/activation_codes/" + str + "/activate");
+        if(str.length === 6) {
+          $.post("/activation_codes/" + str + "/activate");
+        }
+      }
+      else {
+        $(".validation_error").show();
       }
     }
-    else {
-      $(".validation_error").show();
-    }
   }
-}
-
-var getInputString = function() {
-  var inputString = "";
-  $(".code").each(function() {
-    inputString += $.trim($(this).val());
-  });
-  return inputString;
-}
+  
+  $(".code").on('keyup', handleInput);
+});
