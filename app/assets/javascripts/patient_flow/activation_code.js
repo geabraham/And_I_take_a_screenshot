86fe8d30643eeb47da87732d2462938cb1a311a7
Todd <tmartino@mdsol.com>
2014-11-19 13:04:03 -0500

    reverting change to function scope for now

--- app/assets/javascripts/patient_flow/activation_code.js
+++ app/assets/javascripts/patient_flow/activation_code.js
@@ -1,30 +1,30 @@
 $(function() {
-  var getInputString = function() {
-    var inputString = "";
-    $(".code").each(function() {
-      inputString += $.trim($(this).val());
-    });
-    return inputString;
-  }
-  
-  var handleInput = function() {
-    $(this).val($(this).val().toUpperCase());
-    str = getInputString();
-    if( str !== "" ) {
-      var regx = /^[A-Za-z0-9]+$/;
-  
-      if(regx.test(str)) {
-        $(".validation_error").hide();
-    
-        if(str.length === 6) {
-          $.post("/activation_codes/" + str + "/activate");
-        }
-      }
-      else {
-        $(".validation_error").show();
-      }
-    }
-  }
-  
   $(".code").on('keyup', handleInput);
 });
+
+var getInputString = function() {
+  var inputString = "";
+  $(".code").each(function() {
+    inputString += $.trim($(this).val());
+  });
+  return inputString;
+}
+
+var handleInput = function() {
+  $(this).val($(this).val().toUpperCase());
+  str = getInputString();
+  if( str !== "" ) {
+    var regx = /^[A-Za-z0-9]+$/;
+
+    if(regx.test(str)) {
+      $(".validation_error").hide();
+  
+      if(str.length === 6) {
+        $.post("/activation_codes/" + str + "/activate");
+      }
+    }
+    else {
+      $(".validation_error").show();
+    }
+  }
+}
