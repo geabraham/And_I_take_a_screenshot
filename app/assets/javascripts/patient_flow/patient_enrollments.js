4c13a54d7f90dcc573d44099c162902ec45f376c
Todd <tmartino@mdsol.com>
2014-11-24 17:23:03 -0500

    Added password fields validation and rules

--- app/assets/javascripts/patient_flow/patient_enrollments.js
+++ app/assets/javascripts/patient_flow/patient_enrollments.js
@@ -22,16 +22,47 @@ $(function () {
     }
   })
   
+  $.validator.addMethod("pwcheck", function(value) {
+    return  /[a-z]/.test(value) // has a lowercase letter
+      && /[A-Z]/.test(value) // has an uppercase letter
+      && /\d/.test(value) // has a digit
+  });
+  
   $('#next-button').click(function() {
     var currentPage = $('.item.active').attr('id');
+    var form = $('#reg-form');
+    var carousel = $('.carousel');
     
     //TODO check out how much validation to do here; for now just check length/matching
-    if(currentPage === 'email' && $('#reg-form').valid()) {
-       $('.carousel').carousel('next');
-    } else if(currentPage === 'password' && validatePassword()) {
-      $('.carousel').carousel('next');
-    } else if(currentPage === 'security_question' && validateSecurityQuestion()) {
-      $('.carousel').carousel('next');
+    if(currentPage === 'email' && form.valid()) {
+       carousel.carousel('next');
+       addPasswordRules();
+    } else if(currentPage === 'password' && form.valid()) {
+      carousel.carousel('next');
+    } else if(currentPage === 'security_question' && form.valid()) {
+      carousel.carousel('next');
     }
   })
 })
+
+var addPasswordRules = function() {
+  //add validation rules for password fields
+  //this is done dynamically because only part of the larger
+  //form is checked at first
+  $('#patient_enrollment_password').rules('add', {
+    required: true,
+    minlength: 8,
+    pwcheck: true,
+    messages: {
+      required: 'Enter a valid password.',
+      minlength: 'Enter a valid password.',
+      pwcheck: 'Enter a valid password.'
+    }
+  });
+  $('#patient_enrollment_password_confirmation').rules('add', {
+    equalTo: '#patient_enrollment_password',
+    messages: {
+      equalTo: 'Your passwords do not match.'
+    }
+  });
+}
