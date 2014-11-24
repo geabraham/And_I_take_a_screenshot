5732b79545e19d41ece8fafd4a26303ed3df1144
Todd <tmartino@mdsol.com>
2014-11-24 11:18:03 -0500

    reimplement carousel in javascript, add placeholders for validation functions

--- app/assets/javascripts/patient_flow/patient_enrollments.js
+++ app/assets/javascripts/patient_flow/patient_enrollments.js
@@ -1,3 +1,27 @@
-$( "#email" ).on( "dialog", function( event, ui ) {
-  console.log('hey');
-} );
+$(function () {
+  $('.carousel').carousel();
+  
+  $('#next-button').click(function() {
+    var currentPage = $('.item.active').attr('id');
+    
+    if(currentPage === 'email' && validateEmail()) {
+      $('.carousel').carousel('next');
+    } else if(currentPage === 'password' && validatePassword()) {
+      $('.carousel').carousel('next');
+    } else if(currentPage === 'security_question' && validateSecurityQuestion()) {
+      $('.carousel').carousel('next');
+    }
+  })
+})
+
+var validateEmail = function () {
+  return true;
+}
+
+var validatePassword = function () {
+  return true;
+}
+
+var validateSecurityQuestion = function () {
+  return true;
+}
