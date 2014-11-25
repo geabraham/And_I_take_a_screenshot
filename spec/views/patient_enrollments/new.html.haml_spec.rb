a45acde5179999a9a2622537d1010354f920b62d
Todd <tmartino@mdsol.com>
2014-11-25 14:07:41 -0500

    surprisingly involved changes to fix failing spec

--- spec/views/patient_enrollments/new.html.haml_spec.rb
+++ spec/views/patient_enrollments/new.html.haml_spec.rb
@@ -26,14 +26,14 @@ describe 'patient_enrollments/new.html.haml' do
 
   it 'contains a security question dropdown' do
     # TODO: Test that the blank option is selected. "selected: ''" and "selected: nil" didn't work.
-    expect(html).to have_select('Security Question', options: ['', 'What?', 'Who?'])
+    expect(html).to have_select('Security Question', options: ['Security Question', 'What?', 'Who?'])
   end
 
   it 'contains a security answer input' do
-    expect(html).to have_field('Answer', type: 'text', exact: true)
+    expect(html).to have_field('Security Answer', type: 'text', exact: true)
   end
 
   it 'contains a submit button' do
-    expect(html).to have_selector('input[@type="submit"][@value="Submit"]')
+    expect(html).to have_selector('input[@type="submit"][@value="Create account"]')
   end
 end
