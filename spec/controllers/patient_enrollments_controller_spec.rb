ddd1596dc3394daaa87023b890cc93fd3ddf3841
Aimee Barciauskas <abarciauskas@mdsol.com>
2014-12-09 16:03:57 -0500

    Spec for patient enrollment #new with tou dpn agreement code

--- spec/controllers/patient_enrollments_controller_spec.rb
+++ spec/controllers/patient_enrollments_controller_spec.rb
@@ -4,9 +4,17 @@ describe PatientEnrollmentsController do
 
   describe 'GET new' do
     context 'when a patient enrollment uuid is present' do
+      let(:html) { '<html>hi</html>' }
       before do
+        tou_dpn_agreement_body = { html: html }.to_json
+        tou_dpn_agreement_result = double('Google::APIClient::Result').tap do |res|
+          res.stub(:status).and_return(200)
+          res.stub(:body).and_return(tou_dpn_agreement_body)
+        end
         session[:patient_enrollment_uuid] = SecureRandom.uuid
+        Euresource::PatientEnrollment.stub(:invoke).with(:tou_dpn_agreement, {uuid: session[:patient_enrollment_uuid]}).and_return(tou_dpn_agreement_result)
       end
+
       it 'returns success' do
         get :new
         expect(response).to be_success
@@ -23,6 +31,18 @@ describe PatientEnrollmentsController do
 
         get :new
         expect(assigns(:patient_enrollment)).to eq stubbed_enrollment
+      end
+
+      it 'assigns @tou_dpn_agreement_html' do
+        get :new
+        expect(assigns(:tou_dpn_agreement_html)).to eq(html)
+      end
+    end
+
+    context 'when no patient enrollment uuid is present in the request' do
+      it 'returns an error' do
+        get :new
+        expect(response.status).to eq(422)
       end
     end
   end
