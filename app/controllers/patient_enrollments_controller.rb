793bcd3c80189d4c3a23da64b7ad0af3a3d3c826
Aimee Barciauskas <abarciauskas@mdsol.com>
2014-12-09 18:35:06 -0500

    Modify error message

--- app/controllers/patient_enrollments_controller.rb
+++ app/controllers/patient_enrollments_controller.rb
@@ -12,7 +12,7 @@ class PatientEnrollmentsController < ApplicationController
     # NOTE: @security_questions has no test. It is, for now, faked.
     @security_questions = ["What's the worst band in the world?"]
   rescue StandardError => e
-    return render json: {message: 'Unable to continue'}, status: 422
+    return render json: {message: "Unable to continue with registration. Error: #{e.message}"}, status: 422
   end
   
   def create #TODO test this
