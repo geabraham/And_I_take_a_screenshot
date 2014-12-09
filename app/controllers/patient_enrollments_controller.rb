73e9dba635c02f98e21cb621217006e4a911a2f9
Aimee Barciauskas <abarciauskas@mdsol.com>
2014-12-08 19:05:17 -0500

    Basic code for TOU/DPN agreement

--- app/controllers/patient_enrollments_controller.rb
+++ app/controllers/patient_enrollments_controller.rb
@@ -5,6 +5,18 @@ class PatientEnrollmentsController < ApplicationController
   end
 
   def new
+    patient_enrollment_uuid = params[:id].presence || SecureRandom.uuid
+    tou_dpn_agreement_response = Euresource::PatientEnrollment.invoke(:tou_dpn_agreement, {uuid: '04b7207a-49ee-4b35-bd1c-cd35980cd865'})
+
+    unless tou_dpn_agreement_response.status == 200
+      raise "Error trying to retrieve TOU/DPN agreement. #{tou_dpn_agreement_response.status} Response: #{tou_dpn_agreement_response.body}"
+    else
+      if tou_dpn_agreement = JSON.parse(tou_dpn_agreement_response.body)
+        @tou_dpn_agreement_html = tou_dpn_agreement['html']
+      end
+    end
+    # TODO: Add Euresource integration here
+
     # NOTE: @security_questions has no test. It is, for now, faked.
     @security_questions = ["What's the worst band in the world?"]
     @patient_enrollment = PatientEnrollment.new
