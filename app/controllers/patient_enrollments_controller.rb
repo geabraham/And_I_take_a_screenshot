2e25787691c9c151e8abdf25af2766575da94e7d
Todd <tmartino@mdsol.com>
2014-12-12 16:12:05 -0500

    add placeholder logic for user agent check and make download page link to iTunes store by default

--- app/controllers/patient_enrollments_controller.rb
+++ app/controllers/patient_enrollments_controller.rb
@@ -10,14 +10,19 @@ class PatientEnrollmentsController < ApplicationController
   def create #TODO test this
     # TODO PATCH: /v1/patient_enrollments/:patient_enrollment_uuid/register
     
-    #TODO at this point, we should determine whether a user is on a mobile browser
-    #and if so, whether they have the app installed. HOW?
-    #@speedbump_message = 'Open the Patient Cloud app?'
-    
     #if app is not installed
     @speedbump_text = 'Download the Patient Cloud app from the app store?'
     @cancel_text = 'Cancel'
     @proceed_text = 'Open'
+    
+    #TODO: verify that the user agent strings for Patient Cloud iOS/Android agree with this implementation
+    if request.env['HTTP_USER_AGENT'].include?('Patient Cloud iOS')
+      #link to an app URL defined by the iOS team
+    elsif request.env['HTTP_USER_AGENT'].include?('Patient Cloud Android')
+      #link to an app URL defined by the Android team
+    else 
+      @download_link = 'https://itunes.apple.com/us/app/patient-cloud/id554170114?mt=8'
+    end
     
     render 'download'
   end
