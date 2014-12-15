c466e7967f5841bef398484a71cf8f6af9aefe4a
Aimee Barciauskas <abarciauskas@mdsol.com>
2014-12-15 10:20:36 -0500

    Remove unused variable in mauth init file

--- config/initializers/mauth.rb
+++ config/initializers/mauth.rb
@@ -1,7 +1,6 @@
 mauth_yaml_file = File.join(Rails.root, 'config/mauth.yml')
 
 if File.exist?(mauth_yaml_file)
-  config = Rails.application.config
   mauth_conf = YAML.load_file(mauth_yaml_file)[Rails.env]
   MAUTH_CLIENT = MAuth::Client.new(mauth_conf)
 elsif Rails.env.production?
