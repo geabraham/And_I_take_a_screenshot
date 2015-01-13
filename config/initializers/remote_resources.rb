8ae1b10ac4f47e6f6eaaacfe2dfb4859639b6460
Kent Davy <kdavy@mdsol.com>
2015-01-13 15:33:50 -0800

    commented out code in remote_resources which put mock data into non-existant job_titles resource

--- config/initializers/remote_resources.rb
+++ config/initializers/remote_resources.rb
@@ -6,21 +6,27 @@ require 'eureka_tools'
 # api document for the proposed service, and Eureka Tools can fake it for you.
 
 # TODO: remove mocking in production
-if Rails.env.development? || Rails.env.production?
-  [
-      {oid: "PRESIDENT", uuid: "8e19b514-5fa6-11e2-bcfd-0800200c9a66"},
-      {oid: "SOFTWARE_ENGINEER", uuid: "8e19b515-5fa6-11e2-bcfd-0800200c9a66"},
-      {oid: "VICE_PRESIDENT", uuid: "8e19b516-5fa6-11e2-bcfd-0800200c9a66"}
-  ].each do |job_title_attrs|
-    Euresource::JobTitle.post(job_title_attrs)
-  end
-  [
-      {first_name: "John", last_name: "Smith", uuid: "1e19b515-5fa6-11e2-bcfd-0800200c9a69", job_title_uuid: "8e19b514-5fa6-11e2-bcfd-0800200c9a66"},
-      {first_name: "James", last_name: "Jones", uuid: "2e19b515-5fa6-11e2-bcfd-0800200c9a69", job_title_uuid: "8e19b516-5fa6-11e2-bcfd-0800200c9a66"},
-      {first_name: "John", last_name: "Chaplin", uuid: "3e19b515-5fa6-11e2-bcfd-0800200c9a69", job_title_uuid: "8e19b516-5fa6-11e2-bcfd-0800200c9a66"},
-      {first_name: "John", last_name: "Doe", uuid: "4e19b515-5fa6-11e2-bcfd-0800200c9a69", job_title_uuid: "8e19b514-5fa6-11e2-bcfd-0800200c9a66"},
-      {first_name: "Tony", last_name: "Smith", uuid: "5e19b515-5fa6-11e2-bcfd-0800200c9a69", job_title_uuid: "8e19b515-5fa6-11e2-bcfd-0800200c9a66"}
-  ].each do |medidation_attrs|
-    Euresource::Medidation.post(medidation_attrs)
-  end
-end
+
+# I'm not sure exactly what this is
+# for, except that maybe it is supposed to serve as an example of how
+# to do this sort of thing.  It seems that we don't have job_title
+# anymore in our api docs so this complains.  Commenting out for now
+# unless everyone agrees it should be removed entirely.
+#if Rails.env.development? || Rails.env.production?
+#  [
+#      {oid: "PRESIDENT", uuid: "8e19b514-5fa6-11e2-bcfd-0800200c9a66"},
+#      {oid: "SOFTWARE_ENGINEER", uuid: "8e19b515-5fa6-11e2-bcfd-0800200c9a66"},
+#      {oid: "VICE_PRESIDENT", uuid: "8e19b516-5fa6-11e2-bcfd-0800200c9a66"}
+#  ].each do |job_title_attrs|
+#    Euresource::JobTitle.post(job_title_attrs)
+#  end
+#  [
+#      {first_name: "John", last_name: "Smith", uuid: "1e19b515-5fa6-11e2-bcfd-0800200c9a69", job_title_uuid: "8e19b514-5fa6-11e2-bcfd-0800200c9a66"},
+#      {first_name: "James", last_name: "Jones", uuid: "2e19b515-5fa6-11e2-bcfd-0800200c9a69", job_title_uuid: "8e19b516-5fa6-11e2-bcfd-0800200c9a66"},
+#      {first_name: "John", last_name: "Chaplin", uuid: "3e19b515-5fa6-11e2-bcfd-0800200c9a69", job_title_uuid: "8e19b516-5fa6-11e2-bcfd-0800200c9a66"},
+#      {first_name: "John", last_name: "Doe", uuid: "4e19b515-5fa6-11e2-bcfd-0800200c9a69", job_title_uuid: "8e19b514-5fa6-11e2-bcfd-0800200c9a66"},
+#      {first_name: "Tony", last_name: "Smith", uuid: "5e19b515-5fa6-11e2-bcfd-0800200c9a69", job_title_uuid: "8e19b515-5fa6-11e2-bcfd-0800200c9a66"}
+#  ].each do |medidation_attrs|
+#    Euresource::Medidation.post(medidation_attrs)
+#  end
+#end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Minotaur::Application.routes.draw do
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   root to: 'activation_codes#index'
 
-  resources :activation_codes, only: [:index] do
+  resources :activation_codes, only: [:index, :show] do
     get 'activate', on: :member
   end
+
+  get 'activation_code/:activation_code/activate', to: "activation_codes#activate"
 
   resource :patient_enrollments, only: [:new, :create]
 
