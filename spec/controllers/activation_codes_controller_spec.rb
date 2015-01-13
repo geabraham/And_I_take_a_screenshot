7a4ae06b6442da6e52ff99d8be743d3c2694736d
Kent Davy <kdavy@mdsol.com>
2015-01-13 15:35:37 -0800

    Added specs for activation_codes_controller

--- spec/controllers/activation_codes_controller_spec.rb
+++ spec/controllers/activation_codes_controller_spec.rb
@@ -11,13 +11,81 @@ describe ActivationCodesController do
     it_behaves_like 'returns expected status'
     it_behaves_like 'renders expected template'
   end
-  
-  describe "POST 'activate'" do
-    let(:params)               { {id: SecureRandom.uuid} }
-    let(:verb)                 { :post }
-    let(:action)               { :activate }
-    let(:expected_status_code) { 200 }
 
-    it_behaves_like 'returns expected status'
+  describe "GET activate" do
+
+    context 'valid and active activation code' do
+      before do
+        Euresource::ActivationCodes.stub(:get).with(anything()) {
+          Euresource::ActivationCodes.new(attributes: {"activation_code" => '123',
+                                                       "state" => 'active',
+                                                       "patient_enrollment_uuid" => 'xyz'})}
+      end
+
+      let(:params)               { {activation_code: '123'} }
+      let(:verb)                 { :get }
+      let(:action)               { :activate }
+      let(:expected_status_code) { 200 }
+
+      it_behaves_like 'returns expected status'
+
+      it 'adds activation_code and patient_enrollment_uuid to session' do
+        get(:activate, {activation_code: '123'})
+        expect(session['activation_code']).to eq('123')
+        expect(session['patient_enrollment_uuid']).to eq('xyz')
+      end
+
+      context 'valid but deactivated activation code' do
+        before do
+          Euresource::ActivationCodes.stub(:get).with(anything()) {
+            Euresource::ActivationCodes.new(attributes: {"activation_code" => '123',
+                                                         "state" => 'deactivated',
+                                                         "patient_enrollment_uuid" => 'xyz'})}
+        end
+
+        let(:params)               { {activation_code: '123'} }
+        let(:verb)                 { :get }
+        let(:action)               { :activate }
+        let(:error_response_body)  { "Activation Code must be in active state"}
+        let(:expected_status_code) { 403 }
+
+        it_behaves_like 'returns expected status'
+        it_behaves_like 'returns expected error response body'
+
+        it 'does not add activation_code and patient_enrollment_uuid to session' do
+          get(:activate, {activation_code: '123'})
+          expect(session['activation_code']).to eq(nil)
+          expect(session['patient_enrollment_uuid']).to eq(nil)
+        end
+      end
+
+      context 'invalid activation code' do
+        before do
+          Euresource::ActivationCodes.stub(:get).with(anything()) {
+            raise Euresource::ResourceNotFound.new(404,"Failed.  Response status: 404.")
+            Euresource::ActivationCodes.new(attributes: {"activation_code" => '',
+                                                         "state" => 'active',
+                                                         "patient_enrollment_uuid" => 'xyz'})}
+        end
+
+        let(:params)               { {activation_code: '123'} }
+        let(:verb)                 { :get }
+        let(:action)               { :activate }
+        let(:error_response_body)  { "Failed.  Response status: 404."}
+        let(:expected_status_code) { 404 }
+
+        it_behaves_like 'returns expected status'
+        it_behaves_like 'returns expected error response body'
+
+        it 'does not add activation_code and patient_enrollment_uuid to session' do
+          get(:activate, {activation_code: '123'})
+          expect(session['activation_code']).to eq(nil)
+          expect(session['patient_enrollment_uuid']).to eq(nil)
+        end
+      end
+
+      end
+
   end
+
 end
