04e93334a2a7d8a482cbb7b5f14984bc6e96b823
Aimee Barciauskas <abarciauskas@mdsol.com>
2015-01-14 18:05:45 -0500

    Fix comment for study sites

--- lib/imedidata/requests/study_sites_request.rb
+++ lib/imedidata/requests/study_sites_request.rb
@@ -1,14 +1,12 @@
-# request = IMedidataClient::StudySitesRequest.new(user_uuid: '06acf77e-c2fe-4bcd-b44a-dd2fea8bd1a3')
+# request = IMedidataClient::StudySitesRequest.new(user_uuid: '0e39dd40-9fe2-11df-a531-12313900d531', study_uuid: 'fda08b50-9fe1-11df-a531-12313900d531')
 #
 # pp JSON.parse(request.response.body)
-# {"studies"=>
-#   [{"name"=>"AimeeLocalTestStudy",
-#     "uuid"=>"276329b5-ba21-455f-afb3-c892a45f4f5f",
-#     "href"=>
-#      "http://localhost:3001/api/v2/studies/276329b5-ba21-455f-afb3-c892a45f4f5f",
-#     "parent_uuid"=>"b715db56-7f28-4b74-89f4-b502cf5a1d45",
-#     "created_at"=>"2015/01/08 18:18:16 +0000",
-#     "updated_at"=>"2015/01/12 20:28:41 +0000"}]}
+# {"study_sites"=>
+#   [{"name"=>"Alfred Hospital",
+#     "number"=>"1234",
+#     "site_number"=>"1234",
+#     "uuid"=>"161332d2-9fe2-11df-a531-12313900d531",
+#     ...
 #
 module IMedidataClient
   class StudySitesRequest < Request
