a57d09d3e0ce9976ae51539728f5368e9716b2b9
Kent Davy <kdavy@mdsol.com>
2015-01-15 13:08:08 -0800

    removed :show method on activation codes route.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Minotaur::Application.routes.draw do
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   root to: 'activation_codes#index'
 
-  resources :activation_codes, only: [:index, :show] do
+  resources :activation_codes, only: [:index] do
     get 'activate', on: :member
   end
 
