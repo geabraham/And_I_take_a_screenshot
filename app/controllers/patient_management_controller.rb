90dac0979952a2912244bc107d002ef4bc54f4c4
Aimee Barciauskas <abarciauskas@mdsol.com>
2015-01-16 12:56:43 -0500

    Initial html specs

--- app/controllers/patient_management_controller.rb
+++ app/controllers/patient_management_controller.rb
@@ -8,15 +8,8 @@ class PatientManagementController < ApplicationController
   before_filter :authorize_user
 
   def select_study_and_site
-    if params[:study_uuid].present?
-      study = request_study!(study_uuid: params[:study_uuid])['study']
-      @study = [[study['name'], study['uuid']]]
-      study_sites = request_study_sites!(params)['study_sites']
-      @study_sites = study_sites.uniq.collect{|ss| [ss['name'], ss['uuid']]}
-    else
-      studies = request_studies!(params)['studies']
-      @studies = studies.uniq.collect {|s| [s['name'], s['uuid']] }
-    end
+    @study_or_studies = studies_selection_list
+    @study_sites = study_sites_selection_list
     render 'select_study_and_site'
   rescue IMedidataClient::IMedidataClientError => e
     render json: {errors: "You are not authorized for patient management. #{e.message}"}, status: :unauthorized
@@ -34,6 +27,25 @@ class PatientManagementController < ApplicationController
     if CASClient::Frameworks::Rails::Filter.filter(self)
       @user_email = session[:cas_extra_attributes]['user_email']
       params.merge!(user_uuid: session[:cas_extra_attributes]['user_uuid'])
+    end
+  end
+
+  def studies_selection_list
+    if params[:study_uuid].present?
+      study = request_study!(study_uuid: params[:study_uuid])['study']
+      [[study['name'], study['uuid']]]
+    else
+      studies = request_studies!(params)['studies']
+      studies.uniq.collect {|s| [s['name'], s['uuid']] }
+    end
+  end
+
+  def study_sites_selection_list
+    if params[:study_uuid].present?
+      study_sites = request_study_sites!(params)['study_sites']
+      @study_sites = study_sites.uniq.collect{|ss| [ss['name'], ss['uuid']]}
+    else
+      []
     end
   end
 end
