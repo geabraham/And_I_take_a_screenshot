baaa05dad6fd68972f4d2af6d572a18af419afbf
Aimee Barciauskas <abarciauskas@mdsol.com>
2015-01-16 10:44:21 -0500

    Add comment

--- lib/imedidata/client.rb
+++ lib/imedidata/client.rb
@@ -4,6 +4,12 @@ require 'imedidata/request'
 #
 module IMedidataClient
 
+
+  # Defines methods for each request type which returns the parsed response body.
+  # E.g.: request_security_questions!(options)
+  #  If the response status is the expected response status (200), returns the parsed security questions response.
+  #  If the response status is not the expected response status, raises an IMedidataClient error with an informative message.
+  #
   ['security_questions', 'studies', 'study_sites'].each do |request_type|
     define_method("request_#{request_type}!") do |options|
       request = "IMedidataClient::#{"#{request_type}_request".titleize.gsub(' ','')}".constantize.new(options)
