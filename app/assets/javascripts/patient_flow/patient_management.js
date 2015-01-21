8c548ea2b358b26a8d07ac2c99b82ddd3cc7ebb6
Aimee Barciauskas <abarciauskas@mdsol.com>
2015-01-20 21:46:19 -0500

    Add comments to patient_management.js

--- app/assets/javascripts/patient_flow/patient_management.js
+++ app/assets/javascripts/patient_flow/patient_management.js
@@ -1,3 +1,5 @@
+// Add an on change handler to each dropdown item study and study site.
+//
 $(function() {
   $("#patient_management_study").on('change', function() {
     study_uuid = this.value;
@@ -16,16 +18,25 @@ $(function() {
   $('#patient_management_study_site').on('change', readyForLaunch)
 });
 
+// Clear study sites populated in the study sites dropdown.
+// Called whenever the study dropdown changes.
 var clearPopulatedStudySites = function() {
   $('#patient_management_study_site').empty().append('<option value>Site</option>');
 }
 
+// Populate the study sites drop down with the list studySites.
+// Called whenever the study dropdown changes after studySites are fetched from /study_sites
+//
 var populateStudySitesDropdown = function(studySites) {
   $.each(studySites, function(index, value) {
     $('#patient_management_study_site').append('<option value=' + value[1] + '>' + value[0] + '</option>')
   });
 }
 
+// Gets the values for the selected study and study site options.
+// If both are non-null, adds an href attribute and enables the launch button.
+// If either one is null, removes the href attribute and disables the launch button.
+//
 var readyForLaunch = function() {
   study_uuid = $('#patient_management_study option:selected')[0].value
   if ($('#patient_management_study_site option:selected')[0]) {
@@ -37,7 +48,7 @@ var readyForLaunch = function() {
   if(!$.isEmptyObject(study_uuid) && !$.isEmptyObject(study_site_uuid)) {
     $('a#launch-link').attr('href', '/patient_management?study_uuid=' + study_uuid + '&study_site_uuid=' + study_site_uuid)
     $('input#launch-patient-management').removeAttr('disabled')
-  } else if(!$('input#launch-patient-management').attr('disabled')) {
+  } else {
     $('a#launch-link').removeAttr('href')
     $('input#launch-patient-management').attr('disabled', 'disabled')
   }
