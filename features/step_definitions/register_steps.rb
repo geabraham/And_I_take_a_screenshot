1b0144caebce42f5f9b01df5c8d7d1a729d50af9
Aimee Barciauskas <abarciauskas@mdsol.com>
2015-01-30 11:48:01 -0500

    Move all instance vars to the Before block

--- features/step_definitions/register_steps.rb
+++ features/step_definitions/register_steps.rb
@@ -2,8 +2,6 @@ Before do
   @security_questions = [{name: 'What year were you born?', id: '1'},
                          {name: 'Last four digits of SSN or Tax ID number?', id: '2'},
                          {name: 'What is your father\'s middle name?', id: '3'}]
-  allow(RemoteSecurityQuestions).to receive(:find_or_fetch).and_return(@security_questions)
-
   @patient_enrollment_uuid = SecureRandom.uuid
   @activation_code = ([*('A'..'Z'),*('0'..'9')] - %w(I O 1 0)).sample(6).join
   @activation_code_attrs = {
@@ -16,11 +14,23 @@ Before do
     html: '<html><body>We think in generalities, but we live in detail.</body></html>', 
     language_code: 'eng'
   }.stringify_keys
+  @patient_enrollment ||= build :patient_enrollment, uuid: @patient_enrollment_uuid, login: 'the-dude@mdsol.com', 
+    password: 'B0wl11ng', answer: 'The Eagles', activation_code: @activation_code
+  @security_question = @security_questions.sample
+  @patient_enrollment_register_params = {
+    login: @patient_enrollment.login,
+    password: @patient_enrollment.password,
+    activation_code: @activation_code,
+    security_question_id: @security_question[:id],
+    answer: @patient_enrollment.answer,
+    tou_accepted_at: /[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} [-|\+|][0-9]{4}/
+  }
 end
 
 When(/^I fill in (a|an) (valid|invalid) activation code$/) do |_, validity|
   activation_code_response = if validity == 'valid'
     allow_any_instance_of(PatientEnrollment).to receive(:tou_dpn_agreement).and_return(@tou_dpn_agreement)
+    allow(RemoteSecurityQuestions).to receive(:find_or_fetch).and_return(@security_questions)
     double('activation_code').tap do |ac|
       allow(ac).to receive(:attributes).and_return(@activation_code_attrs)
     end
@@ -51,19 +61,6 @@ When(/^I accept the TOU\/DPN$/) do
 end
 
 When(/^I submit registration info as a new subject$/) do
-  @patient_enrollment ||= build :patient_enrollment, uuid: @patient_enrollment_uuid, login: 'the-dude@mdsol.com', 
-    password: 'B0wl11ng', answer: 'The Eagles', activation_code: @activation_code
-
-  @security_question = @security_questions.sample
-  @patient_enrollment_register_params = {
-    login: @patient_enrollment.login,
-    password: @patient_enrollment.password,
-    activation_code: @activation_code,
-    security_question_id: @security_question[:id],
-    answer: @patient_enrollment.answer,
-    tou_accepted_at: /[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} [-|\+|][0-9]{4}/
-  }
-
   fill_in 'Email', with: @patient_enrollment.login
   fill_in 'Re-enter Email', with: @patient_enrollment.login.upcase
   # FIXME.
