ce20d5e9537a63828363ab84f38716c6fd38087e
Aimee Barciauskas <abarciauskas@mdsol.com>
2015-02-11 16:15:04 -0500

    Path implementation of ajax for available subjects

--- app/assets/javascripts/patient_management/invitation_form.js
+++ app/assets/javascripts/patient_management/invitation_form.js
@@ -8,11 +8,17 @@ $(function() {
   })
 
   $("#invite-form").on("ajax:success", function(e, data, status, xhr) {
+    console.log(e)
+    console.log(data)
+    console.log(status)
+    console.log(xhr)
     resetErrors()
+    requestAvailableSubjects();
   }).on("ajax:error", function(e, xhr, status, error) {
     resetErrors()
     $("#invite-form-error").removeClass('hidden')
     $("#invite-form-error span.message").append(xhr.responseText)
+    requestAvailableSubjects();
   });
 
   $('#error-x-button').on('click', function() {
@@ -34,4 +40,23 @@ var inviteButtonEnabledDisabled = function() {
 var resetErrors = function() {
   $("#invite-form-error").addClass('hidden')
   $("#invite-form-error span.message").text('')
+}
+
+var requestAvailableSubjects = function() {
+  var queryObj = {}
+  var querystring = location.search.replace('?', '').split('&');
+  for (var i = 0; i < querystring.length; i++) {
+      var name = querystring[i].split('=')[0];
+      var value = querystring[i].split('=')[1];
+      queryObj[name] = value;
+  }
+  $.getJSON('/patient_management/available_subjects?study_uuid=' + queryObj.study_uuid + '&study_site_uuid=' + queryObj.study_site_uuid)
+  .done(function(availableSubjects) {
+    $('#patient_enrollment_subject').empty().append('<option value>Subject</option>');
+    var options = ''
+    $.each(availableSubjects, function(index, value) {
+      options += '<option value=' + value[1] + '>' + value[0] + '</option>'
+    });
+    $('#patient_enrollment_subject').append(options)
+  });
 }
