7672456adef4de08c6a753a3880052ed00d43821
Aimee Barciauskas <abarciauskas@mdsol.com>
2015-02-11 14:53:59 -0500

    Add available subjects route

--- spec/routing/patient_management_route_spec.rb
+++ spec/routing/patient_management_route_spec.rb
@@ -12,4 +12,11 @@ describe 'PatientManagement Routes' do
         controller: 'patient_management',
         action: 'invite')
   end
+
+  it 'has an available subjects route' do
+    study_uuid = 'f9d3a61c-5857-4b74-adce-723897c58fca'
+    study_site_uuid = 'c5a26c85-6acc-42b5-b9b9-c366194aa7f0'
+    path = "/patient_management/available_subjects?study_uuid=#{study_uuid}&study_site_uuid=#{study_site_uuid}"
+    expect(get(path)).to route_to(controller: 'patient_management', action: 'available_subjects', study_uuid: study_uuid, study_site_uuid: study_site_uuid)
+  end
 end
