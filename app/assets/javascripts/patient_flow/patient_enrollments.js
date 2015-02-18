22fd8a3539657365bf2e66bccfb40c5d8d549032
Bernardo Zuniga <bzuniga@mdsol.com>
2015-02-18 15:37:39 -0500

    show errors, step thru with plain jQuery, advance progress bar with plain jQuery

--- app/assets/javascripts/patient_flow/patient_enrollments.js
+++ app/assets/javascripts/patient_flow/patient_enrollments.js
@@ -6,9 +6,9 @@ $(function () {
   $('#next-button').on('click', function(e) {
     e.preventDefault();
     //hide landing page
-    $('#landing_page').hide();
+    $('#landing_page').hide().toggleClass('active');
     //show agreement
-    $('#tou_dpn_agreement').show();
+    $('#tou_dpn_agreement').show().toggleClass('active');
     $('.progress-indicator').removeClass('hidden');
   });
 
@@ -18,59 +18,73 @@ $(function () {
     
     // confirm agreement
     var proceed = confirm("You acknowledge that you have read, understood, and agree to be bound by the Terms of Use and the Privacy Policy referenced herein.");
-    if (proceed === true) {
+    if (proceed) {
       // hide agreement page
-      $('#tou_dpn_agreement').hide();
+      $('#tou_dpn_agreement').hide().toggleClass('active');
       //show email page
-      $('#email').show();
+      $('#email').show().toggleClass('active');
+      $('.progress-indicator .step:eq(1)').removeClass('incomplete').addClass('default');
+      
+      $('#patient_enrollment_login, #patient_enrollment_login_confirmation').on('keyup', function() {
+        if($form.valid()) {
+          $('#next-email').removeClass('disabled');
+        }
+      });
     }
   });
+
   // email
   $('#next-email').on('click', function(e) {
-    e.preventDefault();
-
-    // validate email fields
     
-    if( !$form.valid() ) {
-     // hide email page
-      $('#email').hide();
-      //show password page
-      $('#password').show(); 
-    } else {
-      // show errors
-    }
-
-  })
+    e.preventDefault();
+   // hide email page
+    $('#email').hide().toggleClass('active');
+    //show password page
+    $('#password').show().toggleClass('active'); 
+    $('.progress-indicator .step:eq(2)').removeClass('incomplete').addClass('default');
+    addPasswordRules();
+    $('#patient_enrollment_password, #patient_enrollment_password_confirmation').on('keyup', function() {
+      if($form.valid()) {
+        $('#next-password').removeClass('disabled');
+      }
+    });
+  });
 
   // password
   $('#next-password').on('click', function(e) {
     e.preventDefault();
-    log('password next')
-    $('#password').hide();
+    $('#password').hide().toggleClass('active');
       //show password page
-    $('#security_question').show(); 
+    $('#security_question').show().toggleClass('active');
+    $('.progress-indicator .step:eq(3)').removeClass('incomplete').addClass('default');
+
+    $('#patient_enrollment_answer').on('keyup', function() {
+      
+    });
+
   });
 
   // security question
   $('#create-account').on('click', function(e) {
+    
     e.preventDefault();
-    log('create-account')
-    $('#password').hide();
+    console.log('create account')
+    // $('#password').hide().toggleClass('active');
       //show password page
-    $('#security_question').show(); 
+    // $('#security_question').show().toggleClass('active');
+
   });
 
-  $('.back_arrow').on('click', backClick);
+  // $('.back_arrow').on('click', backClick);
 
-  $('#patient_enrollment_answer').on('keyup', answerKeyup);
   
-  $('#patient_enrollment_security_question').on('textchange', questionChange);
+  // $('#patient_enrollment_security_question').on('textchange', questionChange);
   
-  $(document).keypress(function(e){
-      if (e.which == 13){
-          $("#next-button").click();
-      }
-  });
+  // $(document).keypress(function(e){
+  //     if (e.which == 13){
+  //         $("#next-button").click();
+  //     }
+  // });
 
 });
 
@@ -82,17 +96,92 @@ var validateSecurityQuestions = function() {
           $('#patient_enrollment_security_question').val() !== '');
 }
 
-var getCurrentPage = function() {
-  return $('.item.active').attr('id')
-}
 
-var confirmTerms = function() {
-  var proceed = confirm("You acknowledge that you have read, understood, and agree to be bound by the Terms of Use and the Privacy Policy referenced herein.");
+
+var backClick = function() {
+  var currentPage = getCurrentPage();
   
-  if (proceed === true) {
-    advanceToEmailPage();
+  if (currentPage !== 'landing_page' && currentPage !== 'tou_dpn_agreement' && currentPage !== 'email') {
+    //TODO currently we are validating on back button click
+    //to prevent a confusing UX issue where the error message
+    //disappears (on back click) and cannot be restored
+    //unless the form is corrected and rebroken
+    //there might be a better workaround, discuss
+    if($('#reg-form').valid() || isBlankEntry()) {
+      hideErrors();
+      reverseProgressBar();
+      $('.carousel').carousel('prev');
+      if (currentPage === 'password') {
+        $('.back_arrow').addClass('hidden');
+      } else if (currentPage === 'security_question') {
+        $('#create-account').addClass('hidden');
+        $('#next-button').removeClass('hidden');
+      }
+    }
   }
 }
+
+
+
+  var answerKeyup = function() {
+
+    if (validateSecurityQuestions() && $('#reg-form').valid()) {
+      $('#create-account').removeAttr('disabled');
+    } else {
+      $('#create-account').attr('disabled', 'disabled');
+    }
+  }
+
+var questionChange = function() {
+  if (validateSecurityQuestions() && $('#reg-form').valid()) {
+    $('#create-account').removeAttr('disabled').removeClass('disabled');
+  } else {
+    $('#create-account').attr('disabled', true).addClass('disabled');
+  }
+}
+
+var hideErrors = function() {
+  $('.active .validation_error').addClass('invisible');
+  $('.active .registration-input, .active label').removeClass('invalid');
+}
+
+var isBlankEntry = function() {
+  return (($('.active .registration-input').first().val().length === 0) 
+       && ($('.active .registration-input').last().val().length === 0));
+}
+
+// ----------------------------------------------------------------------------------------
+var progressBar = {
+  advance: function() {
+
+  },
+  reverse: function() {
+
+  }
+}
+
+var advanceProgressBar = function() {
+  $('.progress-bar-incomplete').first().addClass('progress-bar-default');
+  $('.progress-bar-incomplete').first().removeClass('progress-bar-incomplete');
+}
+
+var reverseProgressBar = function() {
+  $('.progress-bar-default').last().addClass('progress-bar-incomplete');
+  $('.progress-bar-default').last().removeClass('progress-bar-default');
+}
+// ----------------------------------------------------------------------------------------
+
+// var getCurrentPage = function() {
+//   return $('.item.active').attr('id')
+// }
+
+// var confirmTerms = function() {
+//   var proceed = confirm("You acknowledge that you have read, understood, and agree to be bound by the Terms of Use and the Privacy Policy referenced herein.");
+  
+//   if (proceed === true) {
+//     advanceToEmailPage();
+//   }
+// }
 
 // var advanceToEmailPage = function() {
 //   $('#agree-button').addClass('hidden');
@@ -102,13 +191,6 @@ var confirmTerms = function() {
 //   advanceCarousel();
 // }
 
-var advanceCarousel = function () {
-  $('.carousel').carousel('next');
-  
-  //fix for mobile field focus cutting off top of page
-  window.scrollTo(0, 0);
-  document.body.scrollTop = 0;
-}
 
 // var nextButtonClick = function() {
 //   var currentPage = getCurrentPage();
@@ -140,62 +222,3 @@ var advanceCarousel = function () {
 //     }
 //   }
 // }
-
-var backClick = function() {
-  var currentPage = getCurrentPage();
-  
-  if (currentPage !== 'landing_page' && currentPage !== 'tou_dpn_agreement' && currentPage !== 'email') {
-    //TODO currently we are validating on back button click
-    //to prevent a confusing UX issue where the error message
-    //disappears (on back click) and cannot be restored
-    //unless the form is corrected and rebroken
-    //there might be a better workaround, discuss
-    if($('#reg-form').valid() || isBlankEntry()) {
-      hideErrors();
-      reverseProgressBar();
-      $('.carousel').carousel('prev');
-      if (currentPage === 'password') {
-        $('.back_arrow').addClass('hidden');
-      } else if (currentPage === 'security_question') {
-        $('#create-account').addClass('hidden');
-        $('#next-button').removeClass('hidden');
-      }
-    }
-  }
-}
-
-var advanceProgressBar = function() {
-  $('.progress-bar-incomplete').first().addClass('progress-bar-default');
-  $('.progress-bar-incomplete').first().removeClass('progress-bar-incomplete');
-}
-
-var reverseProgressBar = function() {
-  $('.progress-bar-default').last().addClass('progress-bar-incomplete');
-  $('.progress-bar-default').last().removeClass('progress-bar-default');
-}
-
-var answerKeyup = function() {
-  if (validateSecurityQuestions() && $('#reg-form').valid()) {
-    $('#create-account').removeAttr('disabled');
-  } else {
-    $('#create-account').attr('disabled', 'disabled');
-  }
-}
-
-var questionChange = function() {
-  if (validateSecurityQuestions() && $('#reg-form').valid()) {
-    $('#create-account').removeAttr('disabled');
-  } else {
-    $('#create-account').attr('disabled', 'disabled');
-  }
-}
-
-var hideErrors = function() {
-  $('.active .validation_error').addClass('invisible');
-  $('.active .registration-input, .active label').removeClass('invalid');
-}
-
-var isBlankEntry = function() {
-  return (($('.active .registration-input').first().val().length === 0) 
-       && ($('.active .registration-input').last().val().length === 0));
-}
