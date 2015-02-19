6b4568881ba8cd4b4e159f67963c5f46e242bf5b
Kent Davy <kdavy@mdsol.com>
2015-02-19 15:38:03 -0800

    Get language_code from activation_code call and add to session

--- app/controllers/activation_codes_controller.rb
+++ app/controllers/activation_codes_controller.rb
@@ -7,6 +7,7 @@ class ActivationCodesController < ApplicationController
       if @activation_code.attributes['state'] == 'active'
         session[:patient_enrollment_uuid] = @activation_code.attributes['patient_enrollment_uuid']
         session[:activation_code] = @activation_code.attributes['activation_code']
+        session[:language_code] = @activation_code.attributes['language_code']
         redirect_to controller: :patient_enrollments, action: :new
       else
         render json: "Activation Code must be in active state", status: 422
