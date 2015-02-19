ab2b62408873956947d4a08afd185563a5febb4f
Kent Davy <kdavy@mdsol.com>
2015-02-19 15:39:06 -0800

    Fix patient enrollment specs to match strings in eng.yml

--- spec/views/patient_enrollments/new.html.haml_spec.rb
+++ spec/views/patient_enrollments/new.html.haml_spec.rb
@@ -9,13 +9,13 @@ describe 'patient_enrollments/new.html.haml' do
     assign(:patient_enrollment_uuid, SecureRandom.uuid)
     render
   end
-  
+
   context 'carousel markup' do
     it 'has auto-scroll disabled' do
       expect(html).to have_selector('div.carousel.slide#registration-details[@data-interval="false"]')
     end
   end
-  
+
   context 'tou_dpn_agreement page' do
     it 'contains tou dpn agreement' do
       expect(html).to have_text('Consider yourself warned.')
@@ -26,11 +26,11 @@ describe 'patient_enrollments/new.html.haml' do
     it 'contains an email input' do
       expect(html).to have_field('Email', type: 'email', exact: true)
     end
-    
+
     it 'contains an email confirmation input' do
       expect(html).to have_field('Re-enter Email', type: 'email', exact: true)
     end
-    
+
     it 'contains a validation error div' do
       expect(html).to have_selector('#email div.validation_error', text: '')
     end
@@ -42,14 +42,14 @@ describe 'patient_enrollments/new.html.haml' do
     end
 
     it 'contains a password confirmation input' do
-      expect(html).to have_field('Confirm Password', type: 'password', exact: true)
+      expect(html).to have_field('Re-enter Password', type: 'password', exact: true)
     end
-    
+
     it 'contains a validation error div' do
       expect(html).to have_selector('#password div.validation_error', text: '')
     end
   end
-  
+
   context 'security question page' do
     it 'contains a security question dropdown' do
       # TODO: Test that the blank option is selected. "selected: ''" and "selected: nil" didn't work.
@@ -60,16 +60,16 @@ describe 'patient_enrollments/new.html.haml' do
       expect(html).to have_field('Security Answer', type: 'text', exact: true)
     end
   end
-  
+
   context 'shared controls' do
     it 'contains a submit button' do
-      expect(html).to have_selector('input[@type="submit"][@value="Create account"]')
+      expect(html).to have_selector('input[@type="submit"][@value="Create my account"]')
     end
-  
+
     it 'contains a back arrow' do
       expect(html).to have_selector('a.back_arrow', text: 'Back')
     end
-  
+
     it 'contains an agree button' do
       expect(html).to have_selector('#agree-button', text: 'I agree')
     end
