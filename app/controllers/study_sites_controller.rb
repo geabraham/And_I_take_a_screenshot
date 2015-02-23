2700493d40b3b0bff44a63a7b5fcd98a26852127
Bernardo Zuniga <bzuniga@mdsol.com>
2015-02-23 11:39:10 -0500

    revert controller

--- app/controllers/study_sites_controller.rb
+++ app/controllers/study_sites_controller.rb
@@ -4,15 +4,10 @@ require 'imedidata/client'
 
 class StudySitesController < ApplicationController
   include IMedidataClient
-  # before_filter :authorize_user
+  before_filter :authorize_user
 
   def index
     params.require(:study_uuid)
-    render json: [
-      ['DeepSpaceStation', SecureRandom.uuid],
-      ['GalacticQuadrantBeta', SecureRandom.uuid],
-      ['GenesisPlanet', SecureRandom.uuid],
-      ['StarfleetAcademy', SecureRandom.uuid]
-    ]
+    render json: name_uuid_options_array(request_study_sites!(params)['study_sites'])
   end
 end
