0c3d109445e2d94ed4ad28cd6cf2aa7b4e6c0f76
Aimee Barciauskas <abarciauskas@mdsol.com>
2015-02-23 11:44:19 -0500

    Use symbol

--- app/helpers/patient_management_permissions_helper.rb
+++ app/helpers/patient_management_permissions_helper.rb
@@ -15,7 +15,7 @@ module PatientManagementPermissionsHelper
   def check_study_site_permissions
     Rails.logger.info_with_data("Checking study site authorizations for patient management.", params: params)
     study_sites = request_study_sites!(params)
-    raise_and_log('study_site') unless study_sites.present?
+    raise_and_log(:study_site) unless study_sites.present?
     @study_site = study_sites['study_sites'].find { |ss| ss['uuid'] == params[:study_site_uuid]}
     true
   end
@@ -25,13 +25,13 @@ module PatientManagementPermissionsHelper
     @studies = request_studies!(params)['studies']
     if params[:study_uuid]
       @study = @studies.find {|s| s['uuid'] == params[:study_uuid]}
-      raise_and_log('study') unless @study.present?
+      raise_and_log(:study) unless @study.present?
     end
     true
   end
 
   def raise_and_log(object_type)
-    error_message = no_permissions_error(object_type, object_type == 'study_site' ? params[:study_site_uuid] : params[:study_uuid])
+    error_message = no_permissions_error(object_type, object_type == :study_site ? params[:study_site_uuid] : params[:study_uuid])
     Rails.logger.error_with_data(error_message, params: params)
     raise PermissionsError.new(error_message)
   end
