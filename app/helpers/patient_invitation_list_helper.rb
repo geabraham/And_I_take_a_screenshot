9ff36f7c2b684164dc9cf2d26587f356d9728ee1
Todd Martino <tmartino@mdsol.com>
2015-02-24 17:45:09 -0500

    sort descending by date

--- app/helpers/patient_invitation_list_helper.rb
+++ app/helpers/patient_invitation_list_helper.rb
@@ -5,11 +5,11 @@ module PatientInvitationListHelper
     stubby_return = [{created_at: '2014-02-18 23:08:08', subject_identifier: 'SUB029', email: 'todfsdfsdfdsfsdfsdfdd@blahhahhahahaha.org', initials: 'TM', activation_code: 'GW346J', state: 'Pending'},
                     {created_at: '2015-02-18 11:08:08', subject_identifier: 'SUB030', email: 'chad@hotmail.com', initials: 'CV', activation_code: 'FH8492', state: 'Pending'},
                     {created_at: '2013-02-18 07:08:08', subject_identifier: 'SUB031', email: 'aimee@gmail.com', initials: 'AB', activation_code: 'ASDF98', state: 'Registered'},
-                    {created_at: '2013-02-18 06:08:08', subject_identifier: 'SUB032', email: 'kent@mdsol.com', initials: 'KD', activation_code: 'SFH839', state: 'Pending'}]
+                    {created_at: '2013-01-18 06:08:08', subject_identifier: 'SUB032', email: 'kent@mdsol.com', initials: 'KD', activation_code: 'SFH839', state: 'Pending'}]
                     
-    (1..100).each { |a| stubby_return << {created_at: '2013-02-18 06:08:08', subject_identifier: "dup#{a}", email: "todd#{a}@mdsol.com", initials: "#{a}", activation_code: "SFH#{a}", state: 'Pending'} }
+    (1..100).each { |a| stubby_return << {created_at: '2012-02-18 06:08:08', subject_identifier: "dup#{a}", email: "todd#{a}@mdsol.com", initials: "#{a}", activation_code: "SFH#{a}", state: 'Pending'} }
     # real code below
-    @enrollments = stubby_return
+    @enrollments = stubby_return.sort_by { |e| e[:created_at] }.reverse
     @enrollments.each do |enrollment| 
       enrollment[:email] = anonymize_email(enrollment[:email])
       enrollment[:created_at] = Date.strptime(enrollment[:created_at]).strftime('%d-%^b-%Y')
