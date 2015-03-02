2c099f3914d0dae410a5c29c1b3a59a907bb9be5
Aimee Barciauskas <abarciauskas@mdsol.com>
2015-03-02 13:45:40 -0500

    Specs and minor changes to PatientInvitationListHelper

--- app/helpers/patient_invitation_list_helper.rb
+++ app/helpers/patient_invitation_list_helper.rb
@@ -8,28 +8,29 @@ module PatientInvitationListHelper
   
   def format_and_anonymize(enrollment)
     enrollment.email = anonymize_email(enrollment.email) unless enrollment.email.blank?
-    enrollment.created_at = format_date(enrollment.created_at)
-    enrollment.state = enrollment.state.capitalize
+    enrollment.created_at = format_date(enrollment.created_at) unless enrollment.created_at.blank?
+    enrollment.state = enrollment.state.capitalize unless enrollment.state.blank?
     enrollment
   end
   
   private
   def anonymize_email(email)
-    user = email.split('@').first.chars
+    user, domain = email.split("@")
+    user = user.chars
+    tld = domain.present? ? domain.split('.').from(1).join('.') : ''
+    domain = (domain.present? ? domain.split('.').first : '').chars
+
     if user.length >= 2
       user = user.take(2).join << '*' * (user.length > 20 ? 18 : user.length - 2)
     else
       user = user.take(2).join << '***'
     end
     
-    domain = email.split('@').last.split('.').first.chars
     if domain.length >= 2
       domain = domain.take(2).join << '*' * (domain.length > 10 ? 8 : domain.length - 2)
     else
       domain = domain.take(2).join << '***'
     end
-    
-    tld = email.split('@').last.split('.').from(1).join('.')
     
     "#{user}@#{domain}.#{tld}"
   end
