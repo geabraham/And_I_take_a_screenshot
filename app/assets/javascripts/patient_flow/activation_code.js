518c476d3e94084f8ef3907555985da6d5795642
Todd Martino <tmartino@mdsol.com>
2015-03-09 11:02:56 -0400

    fix to issue with activation code page validation error not displaying

--- app/assets/javascripts/patient_flow/activation_code.js
+++ app/assets/javascripts/patient_flow/activation_code.js
@@ -1,13 +1,9 @@
 $(function() {
-  if ($('#code').length > 0) {
-    handleInput(); // fix for MCC-151106
-  }
-  
   $('#code').focus();
   
   $('#code').on('keyup', handleInput);
   
-  $(document).keypress(function(e) {
+  $(document).keypress(function(e){
       if (e.which == 13){
         $('#activate-button').click();
       }
@@ -34,12 +30,12 @@ var handleInput = function() {
 
     if(str.length === 6) {
       if(regx.test(str)) {
-        $(".validation_error").addClass('invisible');
+        $(".validation_error").hide();
         $(".activation-code").removeClass('has-error');
         $('#activate-button').prop('href', '/activation_codes/' + str + '/validate')
         $('#activate-button').removeClass('disabled');
       } else {
-        $(".validation_error").removeClass('invisible');
+        $(".validation_error").show();
         $(".activation-code").addClass('has-error');
         $('#activate-button').prop('href', '')
         $('#activate-button').addClass('disabled');
